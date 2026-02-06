cc7664a1d112c5766a90510b5f20476429b4d8cc
Itj7 <rahimwael2007@gmail.com>
2026-02-06 17:27:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const evilButton = document.getElementById('evil-button')
+const yesButton = document.querySelector('.yes-button')
+const OFFSET = 100
+let chaseCount = 0
+let baseSize = 40
+
+evilButton.addEventListener('click', () => {
+  alert('Try pressing YES love ❤️')
+  location.reload()
+})
+
+document.addEventListener('mousemove', (e) => {
+  const x = e.pageX
+  const y = e.pageY
+  const buttonBox = evilButton.getBoundingClientRect()
+  const horizontalDistanceFrom = distanceFromCenter(buttonBox.x, x, buttonBox.width)
+  const verticalDistanceFrom = distanceFromCenter(buttonBox.y, y, buttonBox.height)
+  const horizontalOffset = buttonBox.width / 2 + OFFSET
+  const verticalOffset = buttonBox.height / 2 + OFFSET
+  if (Math.abs(horizontalDistanceFrom) <= horizontalOffset && Math.abs(verticalDistanceFrom) <= verticalOffset) {
+    chaseCount++
+    growYesButton()
+    setButtonPosition(
+      buttonBox.x + horizontalOffset / horizontalDistanceFrom * 10,
+      buttonBox.y + verticalOffset / verticalDistanceFrom * 10
+    )
+  }
+})
+
+function growYesButton() {
+  const newSize = baseSize + (chaseCount * 0.5)
+  yesButton.style.fontSize = `${newSize}px`
+  yesButton.style.padding = `${18 + (chaseCount * 0.3)}px ${65 + (chaseCount * 0.3)}px`
+}
+
+function setButtonPosition(left, top) {
+  const windowBox = document.body.getBoundingClientRect()
+  const buttonBox = evilButton.getBoundingClientRect()
+
+  if(distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+    left = windowBox.right - buttonBox.width - OFFSET
+  }
+  if(distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+    left = windowBox.left + OFFSET
+  }
+  if(distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+    top = windowBox.bottom - buttonBox.height - OFFSET
+  }
+  if(distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+    top = windowBox.top + OFFSET
+  }
+
+  evilButton.style.left = `${left}px`
+  evilButton.style.top = `${top}px`
+}
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
+  return boxPosition - mousePosition + boxSize / 2
+}
